16f3e003bd6674c65e8f4b6cf93804bc5f1b5901
Fiaz Sami <fiaz.sami@collecdev.com>
2015-02-03 16:14:22 -0600

    refactored Titan's old code into this project

--- InstaBoss/CameraViewController.h
+++ InstaBoss/CameraViewController.h
@@ -11,3 +11,4 @@
 @interface CameraViewController : UIViewController
 
 @end
+
--- InstaBoss/CameraViewController.m
+++ InstaBoss/CameraViewController.m
@@ -8,6 +8,64 @@
 
 #import "CameraViewController.h"
 
+@interface CameraViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *viewCamera;
+
+@end
+
 @implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self isCameraEnabled];
+}
+
+- (BOOL)isCameraEnabled {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        [self noCameraAlert];
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (void)noCameraAlert {
+    UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Device has no camera" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [myAlertView show];
+}
+
+- (IBAction)tapButtonTakePicture:(UIButton *)sender {
+    if([self isCameraEnabled]) {
+        [self showSourcePicker:UIImagePickerControllerSourceTypeCamera];
+    }
+}
+
+- (IBAction)tapButtonSelectFromGallery:(UIButton *)sender {
+    [self showSourcePicker:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.viewCamera.image = chosenImage;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+- (void)showSourcePicker:(UIImagePickerControllerSourceType)source {
+
+    UIImagePickerController *picker = [UIImagePickerController new];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = source;
+    [self presentViewController:picker animated:YES completion:NULL];
+
+}
+
 
 @end
