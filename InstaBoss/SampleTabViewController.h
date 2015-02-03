5bda8ef74a320aad30e4745c1e763f67c21d485a
Fiaz Sami <fiaz.sami@collecdev.com>
2015-02-03 12:28:16 -0600

    basic icons and tab view setup

---
+++ InstaBoss/SampleTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleTabViewController.h
+//  InstaBoss
+//
+//  Created by Fiaz Sami on 2/3/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleTabViewController : UIViewController
+
+@end
---
+++ InstaBoss/SampleTabViewController.m
@@ -0,0 +1,13 @@
+//
+//  SampleTabViewController.m
+//  InstaBoss
+//
+//  Created by Fiaz Sami on 2/3/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+#import "SampleTabViewController.h"
+
+@implementation SampleTabViewController
+
+@end
