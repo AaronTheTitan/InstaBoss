dec79a92e66b9418971778edaf6344cf83b3199f
Fiaz Sami <fiaz.sami@collecdev.com>
2015-02-05 12:58:33 -0600

    refactored searchBar name

--- InstaBoss/SearchViewController.m
+++ InstaBoss/SearchViewController.m
@@ -6,20 +6,44 @@
 //  Copyright (c) 2015 Aaron Bradley. All rights reserved.
 //
 
+#import <Parse/Parse.h>
+#import "Constants.h"
 #import "SearchViewController.h"
 
 
 @interface SearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
-
 {
-
     IBOutlet UITableView *tableViewSearch;
-    IBOutlet UISearchBar *searchBar;
+    IBOutlet UISearchBar *searchBarInput;
 }
-
 
 @end
 
 @implementation SearchViewController
+{
+    NSArray *searchResults;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    searchResults = [NSArray new];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return searchResults.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell" forIndexPath:indexPath];
+
+    return cell;
+}
+
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    PFQuery *query = [PFQuery queryWithClassName:kParseHashTagObjectClass];
+    
+}
+
 
 @end
