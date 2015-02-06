0438709f4a3243aada221a74dd26b5e926ce1936
Fiaz Sami <fiaz.sami@collecdev.com>
2015-02-06 09:46:05 -0600

    basic follow/unfollow interaction implemented

--- InstaBoss/FriendCell.m
+++ InstaBoss/FriendCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "FriendCell.h"
+static CGFloat const kBounceValue = 20.0f;
 
 @interface FriendCell () <UIGestureRecognizerDelegate>
 
@@ -30,32 +31,180 @@
     self.panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panThisCell:)];
     self.panRecognizer.delegate = self;
     [self.friendContentView addGestureRecognizer:self.panRecognizer];
+
+    [self.friendButton setTintColor:[UIColor greenColor]];
+    
 }
 
 - (void)panThisCell:(UIPanGestureRecognizer *)recognizer {
     switch (recognizer.state) {
         case UIGestureRecognizerStateBegan:
             self.panStartPoint = [recognizer translationInView:self.friendContentView];
-            NSLog(@"Pan Began at %@", NSStringFromCGPoint(self.panStartPoint));
+            self.startingRightLayoutConstraintConstant = self.contentViewRightConstraint.constant;
             break;
         case UIGestureRecognizerStateChanged: {
             CGPoint currentPoint = [recognizer translationInView:self.friendContentView];
             CGFloat deltaX = currentPoint.x - self.panStartPoint.x;
-            NSLog(@"Pan Moved %f", deltaX);
+            BOOL panningLeft = NO;
+            if (currentPoint.x < self.panStartPoint.x) {  //1
+                panningLeft = YES;
+            }
+
+            if (self.startingRightLayoutConstraintConstant == 0) { //2
+                //The cell was closed and is now opening
+                if (!panningLeft) {
+                    CGFloat constant = MAX(-deltaX, 0); //3
+                    if (constant == 0) { //4
+                        [self resetConstraintContstantsToZero:YES notifyDelegateDidClose:NO];
+                    } else { //5
+                        self.contentViewRightConstraint.constant = constant;
+                    }
+                } else {
+                    CGFloat constant = MIN(-deltaX, [self buttonTotalWidth]); //6
+                    if (constant == [self buttonTotalWidth]) { //7
+                        [self setConstraintsToShowAllButtons:YES notifyDelegateDidOpen:NO];
+                    } else { //8
+                        self.contentViewRightConstraint.constant = constant;
+                    }
+                }
+            } else {
+                //The cell was at least partially open.
+                CGFloat adjustment = self.startingRightLayoutConstraintConstant - deltaX; //1
+                if (!panningLeft) {
+                    CGFloat constant = MAX(adjustment, 0); //2
+                    if (constant == 0) { //3
+                        [self resetConstraintContstantsToZero:YES notifyDelegateDidClose:NO];
+                    } else { //4
+                        self.contentViewRightConstraint.constant = constant;
+                    }
+                } else {
+                    CGFloat constant = MIN(adjustment, [self buttonTotalWidth]); //5
+                    if (constant == [self buttonTotalWidth]) { //6
+                        [self setConstraintsToShowAllButtons:YES notifyDelegateDidOpen:NO];
+                    } else { //7
+                        self.contentViewRightConstraint.constant = constant;
+                    }
+                }
+            }
+            
+            self.contentViewLeftConstraint.constant = -self.contentViewRightConstraint.constant; //8
         }
             break;
         case UIGestureRecognizerStateEnded:
-            NSLog(@"Pan Ended");
+            if (self.startingRightLayoutConstraintConstant == 0) { //1
+                //Cell was opening
+                CGFloat halfOfButtonOne = CGRectGetWidth(self.friendButton.frame); //2
+                if (self.contentViewRightConstraint.constant >= halfOfButtonOne) { //3
+                    //Open all the way
+                    [self setConstraintsToShowAllButtons:YES notifyDelegateDidOpen:YES];
+                } else {
+                    //Re-close
+                    [self resetConstraintContstantsToZero:YES notifyDelegateDidClose:YES];
+                }
+            } else {
+                //Cell was closing
+                CGFloat buttonOnePlusHalfOfButton2 = CGRectGetWidth(self.friendButton.frame); //4
+                if (self.contentViewRightConstraint.constant >= buttonOnePlusHalfOfButton2) { //5
+                    //Re-open all the way
+                    [self setConstraintsToShowAllButtons:YES notifyDelegateDidOpen:YES];
+                } else {
+                    //Close
+                    [self resetConstraintContstantsToZero:YES notifyDelegateDidClose:YES];
+                }
+            }
             break;
         case UIGestureRecognizerStateCancelled:
-            NSLog(@"Pan Cancelled");
-            break;
+            if (self.startingRightLayoutConstraintConstant == 0) {
+                //Cell was closed - reset everything to 0
+                [self resetConstraintContstantsToZero:YES notifyDelegateDidClose:YES];
+            } else {
+                //Cell was open - reset to the open state
+                [self setConstraintsToShowAllButtons:YES notifyDelegateDidOpen:YES];
+            }            break;
         default:
             break;
     }
 }
 
+- (void)updateConstraintsIfNeeded:(BOOL)animated completion:(void (^)(BOOL finished))completion {
+    float duration = 0;
+    if (animated) {
+        duration = 0.1;
+    }
+
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [self layoutIfNeeded];
+    } completion:completion];
+}
+
+- (CGFloat)buttonTotalWidth {
+    return CGRectGetWidth(self.frame) - CGRectGetMinX(self.friendButton.frame);
+}
+
+- (void)resetConstraintContstantsToZero:(BOOL)animated notifyDelegateDidClose:(BOOL)notifyDelegate {
+    //TODO: Notify delegate.
+
+    if (self.startingRightLayoutConstraintConstant == 0 &&
+        self.contentViewRightConstraint.constant == 0) {
+        //Already all the way closed, no bounce necessary
+        return;
+    }
+
+    self.contentViewRightConstraint.constant = 0;
+    self.contentViewLeftConstraint.constant = 0;
+
+    [self updateConstraintsIfNeeded:animated completion:^(BOOL finished) {
+        self.contentViewRightConstraint.constant = 0;
+        self.contentViewLeftConstraint.constant = 0;
+
+        [self updateConstraintsIfNeeded:animated completion:^(BOOL finished) {
+            self.startingRightLayoutConstraintConstant = self.contentViewRightConstraint.constant;
+        }];
+    }];
+}
+
+- (void)setConstraintsToShowAllButtons:(BOOL)animated notifyDelegateDidOpen:(BOOL)notifyDelegate {
+    //TODO: Notify delegate.
+
+    //1
+    if (self.startingRightLayoutConstraintConstant == [self buttonTotalWidth] &&
+        self.contentViewRightConstraint.constant == [self buttonTotalWidth]) {
+        return;
+    }
+    //2
+    self.contentViewLeftConstraint.constant = -[self buttonTotalWidth] - kBounceValue;
+    self.contentViewRightConstraint.constant = [self buttonTotalWidth] + kBounceValue;
+
+    [self updateConstraintsIfNeeded:animated completion:^(BOOL finished) {
+        //3
+        self.contentViewLeftConstraint.constant = -[self buttonTotalWidth];
+        self.contentViewRightConstraint.constant = [self buttonTotalWidth];
+
+        [self updateConstraintsIfNeeded:animated completion:^(BOOL finished) {
+            //4
+            self.startingRightLayoutConstraintConstant = self.contentViewRightConstraint.constant;
+        }];
+    }];
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
+}
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+    [self resetConstraintContstantsToZero:NO notifyDelegateDidClose:NO];
+}
+
 - (IBAction)socialize:(id)sender {
+    if([self.friendButton.titleLabel.text isEqualToString:@"Follow"]) {
+        [self.friendButton setTitle:@"Unfollow" forState:UIControlStateNormal];
+                [self.friendButton setTintColor:[UIColor redColor]];
+    } else {
+        [self.friendButton setTitle:@"Follow" forState:UIControlStateNormal];
+        [self.friendButton setTintColor:[UIColor greenColor]];
+    }
     [self.delegate socializeWithUser];
 }
 
